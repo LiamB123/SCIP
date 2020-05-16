48c368661b4fb55c0eb3448af2acbdf941a55446
LiamB123 <liambrowne14@gmail.com>
2020-05-16 13:03:09 +0000

    added blog template, url and first model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost (models.Model) :
+    
+    title = models.CharField(max_length=200, null=False, blank=False)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.utils import timezone
 
 # Create your views here.
 def blog(request):
-    return render (request,"blog.html")
+    return render (request,"blog/blog.html")
